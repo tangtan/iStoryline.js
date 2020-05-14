7f1b8657cde861673ca9ed0f11031d1748a13849
仁钟 李 <996955087@qq.com>
2020-05-15 03:14:53 +0800

    add functions to load .xml file

--- src/js/data/story.js
+++ src/js/data/story.js
@@ -1,8 +1,11 @@
-import { Table } from "./util";
+import { Table } from "./table";
 
 export class Story {
   constructor() {
     this._tableMap = new Map();
+    this._tableMap.set("timeTable", new Table(0));
+    this._tableMap.set("sessionTable", new Table(0));
+    this._tableMap.set("locationTable", new Table(0));
     this._characters = [];
     this._locations = [];
     this._timeStamps = [];
@@ -11,12 +14,97 @@ export class Story {
   /**
    * read xml/json document
    */
-  load(fileUrl, fileType) {}
+  async load(fileUrl, fileType) {
+    if (fileType === "xml") {
+      const xml = await d3.xml(fileUrl);
+      this._parseXml(xml);
+    } else if (fileType === "json") {
+      const json = await d3.json(fileUrl);
+      // this._parseJson(json)
+    } else {
+      console.error("wrong fileType!");
+    }
+  }
+
+  _parseXml(xml) {
+    let story = xml.querySelector("Story");
+    if (story) {
+      let characters = story.querySelector("Characters");
+      characters = characters.querySelectorAll("Character");
+      for (let character of characters) {
+        let name = character.getAttribute("Name");
+        this._characters.push(name);
+        let spans = character.querySelectorAll("Span");
+        for (let span of spans) {
+          let start = span.getAttribute("Start");
+          let end = span.getAttribute("End");
+          this._timeStamps.push(start, end);
+        }
+      }
+      const timeset = new Set(this._timeStamps);
+      this._timeStamps = Array.from(timeset);
+      this._timeStamps.sort();
+      let sessionTable = this._tableMap.get("sessionTable");
+      let timeTable = this._tableMap.get("timeTable");
+      let locationTable = this._tableMap.get("locationTable");
+      for (let table of [sessionTable, timeTable, locationTable]) {
+        table.resize(this._characters.length, this._timeStamps.length);
+      }
+      for (let character of characters) {
+        let spans = character.querySelectorAll("Span");
+        let characterName = character.getAttribute("Name");
+        let characterId = this._characters.indexOf(characterName);
+        for (let span of spans) {
+          let start = span.getAttribute("Start");
+          let timeId = this._timeStamps.indexOf(start);
+          timeTable.replace(characterId, timeId, 1);
+          let sessionId = span.getAttribute("Session");
+          sessionId = parseInt(sessionId);
+          sessionTable.replace(characterId, timeId, sessionId);
+        }
+      }
+      //parse the location part
+      let locations = story.querySelector("Locations");
+      locations = story.querySelectorAll("Location");
+      for (let location of locations) {
+        let locationTable = this._tableMap.get("locationTable");
+        let sessionTable = this._tableMap.get("sessionTable");
+        let timeTable = this._tableMap.get("timeTable");
+        let locationName = location.getAttribute("Name");
+        this._locations.push(locationName);
+        const locationId = this._locations.length - 1;
+        let sessionsInthislocation = location
+          .getAttribute("Sessions")
+          .split(",");
+        sessionsInthislocation = sessionsInthislocation.map(x => parseInt(x));
+        for (let i = 0; i < sessionTable.rows; i++)
+          for (let j = 0; j < sessionTable.cols; j++) {
+            if (
+              timeTable.value(i, j) &&
+              sessionTable.value(i, j) in sessionsInthislocation
+            ) {
+              locationTable.replace(i, j, locationId);
+            }
+          }
+      }
+      console.log(this);
+    } else {
+      console.error("No story in this Url!");
+    }
+  }
 
   /**
    * export xml/json document
    */
-  dump(fileName, fileType) {}
+  dump(fileName, fileType) {
+    if (fileType === "xml") {
+      // dumpXml(xml)
+    } else if (fileType === "json") {
+      // dumpJson(json)
+    } else {
+      console.error("wrong fileType!");
+    }
+  }
 
   /**
    * get table (tableMap)
