7fe17514bf871c217415f97d937e3730660de884
仁钟 李 <996955087@qq.com>
2020-07-10 00:43:29 +0800

    Update greedySort - add getParam - add runAlgorithms - remove sort.js - move DisjointSet.js to dataStruct.js

--- src/js/align/greedyAlign.js
+++ src/js/align/greedyAlign.js
@@ -3,11 +3,17 @@
  * @param {constraints} Object
  */
 
+import { thomsonCrossSectionDependencies } from 'mathjs'
+
 export function greedyAlign(story, constraints) {
-  const param = getParam(story, constraints);
-  const alignTable = runAlgorithms(param);
-  story.setTable("align", alignTable);
+  debugger
+  console.log(story)
+  const param = getParam(story, constraints)
+  const alignTable = runAlgorithms(param)
+  story.setTable('align', alignTable)
 }
+
+function getParam(story, constraints) {}
 
 /**
  * @param {Number[]} list1
@@ -20,17 +26,17 @@ export function greedyAlign(story, constraints) {
  */
 
 export function longestCommonSubstring(list1Length, list2Length, reward) {
-  let totalReward = [];
-  let direction = [];
+  let totalReward = []
+  let direction = []
   reward.forEach(row => {
-    totalReward.push([]);
-    direction.push([]);
-  });
+    totalReward.push([])
+    direction.push([])
+  })
   for (let i = 0; i < list1Length; i++) {
-    reward[i][-1] = 0;
+    reward[i][-1] = 0
   }
   for (let j = 0; j < list2Length; j++) {
-    reward[-1][j] = 0;
+    reward[-1][j] = 0
   }
   for (let i = 0; i < list1Length; i++) {
     for (let j = 0; j < list2Length; j++) {
@@ -38,39 +44,39 @@ export function longestCommonSubstring(list1Length, list2Length, reward) {
       let valueList = [
         totalReward[i - 1][j],
         totalReward[i][j - 1],
-        totalReward[i - 1][j - 1] + reward[i][j]
-      ]; //0:left,1:right,2:align
-      let maxListArg = maxArg(valueList);
-      direction[i][j] = maxListArg;
+        totalReward[i - 1][j - 1] + reward[i][j],
+      ] //0:left,1:right,2:align
+      let maxListArg = maxArg(valueList)
+      direction[i][j] = maxListArg
     }
   }
-  let alignList = [];
-  let leftHead = list1Length;
-  let rightHead = list2Length;
+  let alignList = []
+  let leftHead = list1Length
+  let rightHead = list2Length
   while (rightHead >= 0 && leftHead >= 0) {
     if (direction[i][j] == 2) {
-      alignList[leftHead] = rightHead;
-      leftHead -= 1;
-      rightHead -= 1;
+      alignList[leftHead] = rightHead
+      leftHead -= 1
+      rightHead -= 1
     } else if (direction[i][j] == 1) {
-      rightHead -= 1;
+      rightHead -= 1
     } else if (direction[i][j] == 0) {
-      leftHead -= 1;
+      leftHead -= 1
     } else {
-      break;
+      break
     }
   }
-  return;
+  return
 }
 
 function maxArg(list) {
-  let ans = -1;
-  let maxValue = Number.MIN_SAFE_INTEGER;
+  let ans = -1
+  let maxValue = Number.MIN_SAFE_INTEGER
   for (let i = 0; i < list.length; i++) {
     if (list[i] > maxValue) {
-      maxValue = list[i];
-      ans = i;
+      maxValue = list[i]
+      ans = i
     }
   }
-  return ans;
+  return ans
 }
--- src/js/order/greedySort.js
+++ src/js/order/greedySort.js
@@ -1,11 +1,7 @@
-import {
-  topoSort,
-  dealSetConstraints,
-  getWeight,
-  dealElementConstraints
-} from "../utils/sort";
-
-import { Table } from "../data/table";
+import { Table } from '../data/table'
+import { i, or } from 'mathjs'
+import { constant } from 'lodash'
+import { DisjointSet } from '../utils/dataStruct'
 
 /**
  * @param {Story} story
@@ -13,9 +9,9 @@ import { Table } from "../data/table";
  */
 
 export function greedySort(story, constraints) {
-  const param = getParam(story, constraints);
-  const sortTable = runAlgorithms(param);
-  story.setTable("sort", sortTable);
+  const param = getParam(story, constraints)
+  const sortTable = runAlgorithms(param)
+  story.setTable('sort', sortTable)
 }
 
 /**
@@ -25,19 +21,55 @@ export function greedySort(story, constraints) {
  * @returns {Object}
  */
 function getParam(story, constraints) {
-  console.log(story);
-  let storyCharacterTable = story.getTable("character");
-  let charactersInSessions = [];
-  let initOrder = [];
-  return null;
+  //console.log(story);
+  let storySessionTable = story.getTable('session')
+  let [height, width] = storySessionTable.mat.size()
+  let charaterinSession = []
+  //console.log(height,width)
+  for (let time = 0; time < width; time++) {
+    charaterinSession.push([])
+    //add a time stamp
+    let sessionMap = new Map()
+    let sessionHash = 0
+    for (let characterID = 0; characterID < height; characterID++) {
+      let sessionID = storySessionTable.value(characterID, time)
+      if (sessionID !== 0 && !sessionMap.has(sessionID)) {
+        sessionMap.set(sessionID, sessionHash)
+        sessionHash += 1
+        charaterinSession[time].push([characterID])
+        //add a session at this time stamp
+      } else if (sessionID !== 0) {
+        charaterinSession[time][sessionMap.get(sessionID)].push(characterID)
+      }
+    }
+  }
+  //console.log(charaterinSession)
+  for (let time = 0; time < width; time++) {
+    constraints.push([])
+  }
+  return {
+    charaterinSession,
+    height,
+    width,
+    constraints,
+    //TODO:TODOTODOTODOTODO
+  }
 }
 
 /**
  *
  * @param {Number[]} order
+ * @param {Number} height 多少人
  * @returns {Number[]}
  */
-function order2Weight(order) {}
+function order2mat(order, height) {
+  let ans = []
+  for (let ID = 0; ID < height; ID++) {
+    let orderID = order.indexOf(ID) + 1
+    ans.push(orderID) //0代表不存在
+  }
+  return ans
+}
 
 /**
  *
@@ -45,8 +77,34 @@ function order2Weight(order) {}
  * @returns {Table}
  */
 function runAlgorithms(param) {
-  let ans = new Table();
-  return ans;
+  let ans = new Table()
+  let { height, width, charaterinSession, constraints } = param
+  //console.log(charaterinSession);
+  ans.resize(height, width)
+  let initOrder = constrainedCrossingReduction(
+    charaterinSession[0],
+    charaterinSession[0]
+  )
+  let initOrderMat = order2mat(initOrder, height)
+  //console.log(initOrder)
+  //console.log(initOrderMat)
+  let replaceIndex = []
+  for (let i = 0; i < height; i++) replaceIndex.push(i)
+  ans.replace(replaceIndex, 0, initOrderMat)
+  //console.log(ans)
+  let lastTimeOrder = initOrder
+  for (let time = 1; time < width; time++) {
+    let thisTimeOrder = constrainedCrossingReduction(
+      charaterinSession[time],
+      lastTimeOrder
+    )
+    let thisTimeOrderMat = order2mat(thisTimeOrder, height)
+    //console.log("time",time,thisTimeOrderMat)
+    ans.replace(replaceIndex, time, thisTimeOrderMat)
+    lastTimeOrder = thisTimeOrder
+  }
+  //console.log(ans)
+  return ans
 }
 
 /**
@@ -60,46 +118,152 @@ function runAlgorithms(param) {
  */
 
 export function constrainedCrossingReduction(list1, list2, constraints = []) {
-  let list1Weight = [];
+  let list1Weight = []
   for (let arr of list1) {
-    let sumWeight = 0;
+    let sumWeight = 0
     for (let element of arr) {
-      sumWeight += getWeight(element, list2);
+      sumWeight += getWeight(element, list2)
     }
-    list1Weight.push(sumWeight / arr.length);
+    list1Weight.push(sumWeight / arr.length)
   }
   let [mapSetArr, setInDegree] = dealSetConstraints(
     list1,
     constraints,
     list1Weight
-  );
-  let order = topoSort(mapSetArr, setInDegree, list1Weight);
+  )
+  let order = topoSort(mapSetArr, setInDegree, list1Weight)
   //
   list1.sort((a, b) => {
-    const indexA = list1.indexOf(a);
-    const indexB = list1.indexOf(b);
-    return order.indexOf(indexA) - order.indexOf(indexB);
-  });
-  let allElementWeight = [];
-  let allElement = [];
+    const indexA = list1.indexOf(a)
+    const indexB = list1.indexOf(b)
+    return order.indexOf(indexA) - order.indexOf(indexB)
+  })
+  let allElementWeight = []
+  let allElement = []
   list1.forEach(set => {
     set.forEach(id => {
-      allElement.push(id);
-      allElementWeight.push(getWeight(id, list2));
-    });
-  });
+      allElement.push(id)
+      allElementWeight.push(getWeight(id, list2))
+    })
+  })
   let [mapElementArr, elementInDegree] = dealElementConstraints(
     list1,
     constraints,
     allElementWeight
-  );
-  let elementOrder = topoSort(mapElementArr, elementInDegree, allElementWeight);
+  )
+  let elementOrder = topoSort(mapElementArr, elementInDegree, allElementWeight)
   for (let arr of list1) {
     arr.sort((a, b) => {
-      const indexA = allElement.indexOf(a);
-      const indexB = allElement.indexOf(b);
-      return elementOrder.indexOf(indexA) - elementOrder.indexOf(indexB);
-    });
+      const indexA = allElement.indexOf(a)
+      const indexB = allElement.indexOf(b)
+      return elementOrder.indexOf(indexA) - elementOrder.indexOf(indexB)
+    })
   }
-  return list1;
+  let ans = []
+  list1.forEach(session => ans.push(...session))
+  return ans
+}
+
+//---------------------part of toposort--------------
+export function topoSort(mapSetArr, inDegree, listWeight) {
+  let order = []
+  for (let i = 0; i < inDegree.length; i++) {
+    let minWeight = Number.MAX_SAFE_INTEGER
+    let pos = -1
+    for (let j = 0; j < inDegree.length; j++) {
+      if (inDegree[j] == 0 && listWeight[j] < minWeight) {
+        pos = j
+        minWeight = listWeight[j]
+      }
+    }
+    order.push(pos)
+    inDegree[pos] = -1
+    //has been added to the order array
+    for (let i = 0; i < mapSetArr[pos].length; i++) {
+      if (mapSetArr[pos][i] !== undefined) {
+        inDegree[i] -= 1
+      }
+    }
+  }
+  return order
+}
+
+export function dealSetConstraints(list, constraints, listWeight) {
+  let mapSetArr = []
+  //transform constraint to edges in a graph
+  let inDegree = []
+  let setArr = new DisjointSet(list.length)
+  //binding sets
+  list.forEach(x => {
+    mapSetArr.push([])
+    inDegree.push(0)
+  })
+  for (let constraint of constraints) {
+    let [first, second] = constraint
+    let firstSetId = getSetId(first, list)
+    let secondSetId = getSetId(second, list)
+    if (firstSetId !== secondSetId) {
+      setArr.union(firstSetId, secondSetId)
+      let sumWeight = 0
+      let allElement = setArr.allElementInSet(firstSetId)
+      allElement.forEach(id => (sumWeight += listWeight[id]))
+      for (let i = 0; i < allElement.length; i++) {
+        listWeight[allElement[i]] = sumWeight / allElement.length
+      }
+      mapSetArr[firstSetId][secondSetId] = 1
+      inDegree[secondSetId] += 1
+    }
+  }
+  return [mapSetArr, inDegree]
+}
+
+export function getWeight(id, list2) {
+  return list2.indexOf(id)
+}
+
+export function getSetId(element, setArr) {
+  for (let set of setArr) {
+    if (set.indexOf(element) !== -1) {
+      return setArr.indexOf(set)
+    }
+  }
+  console.error("Can't find this element in any set!")
+  return -1
+}
+
+export function dealElementConstraints(list, constraints, listWeight) {
+  let listAllElement = []
+  list.forEach(x => {
+    listAllElement = [...listAllElement, ...x]
+  })
+  let mapElementArr = []
+  let inDegree = []
+  let setArr = new DisjointSet(listAllElement.length)
+
+  listAllElement.forEach(x => {
+    mapElementArr.push([])
+    inDegree.push(0)
+  })
+  for (let constraint of constraints) {
+    let [first, second] = constraint
+    let firstSetId = getSetId(first, list)
+    let secondSetId = getSetId(second, list)
+    if (firstSetId === secondSetId) {
+      setArr.union(
+        listAllElement.indexOf(first),
+        listAllElement.indexOf(second)
+      )
+      let allElement = setArr.allElementInSet(listAllElement.indexOf(first))
+      let sumWeight = 0
+      allElement.forEach(id => (sumWeight += listWeight[id]))
+      for (let i = 0; i < allElement.length; i++) {
+        listWeight[allElement[i]] = sumWeight / allElement.length
+      }
+      mapElementArr[listAllElement.indexOf(first)][
+        listAllElement.indexOf(second)
+      ] = 1
+      inDegree[listAllElement.indexOf(second)] += 1
+    }
+  }
+  return [mapElementArr, inDegree]
 }
