bec8e33e9c24790ec2ede1304fbc0ad7e60c48f7
仁钟 李 <996955087@qq.com>
2020-07-10 20:07:26 +0800

    Update greedyAlign.js - add getParam - add runAlgorithms Update greedySlotCopact.js - add getParam - add runAlgorithms - only for test,  not finish

--- src/js/align/greedyAlign.js
+++ src/js/align/greedyAlign.js
@@ -1,20 +1,140 @@
+import { thomsonCrossSectionDependencies } from 'mathjs'
+import { Table } from '../data/table'
+
+const alpha = 0.1 //in storyflow paper, a param to measures how similar relative positions are in two frames
+
 /**
  * @param {Story} story
  * @param {constraints} Object
  */
-
-import { thomsonCrossSectionDependencies } from 'mathjs'
-
 export function greedyAlign(story, constraints) {
-  debugger
-  console.log(story)
+  //console.log(story.getTable("sort"))
+  let list1Length = 4
+  let list2Length = 3
+  let reward = [[1, 1, 100], [1, 1, 1], [1, 1, 1], [1, 1, 1]]
+  //console.log(longestCommonSubstring(list1Length,list2Length,reward));
   const param = getParam(story, constraints)
   const alignTable = runAlgorithms(param)
   story.setTable('align', alignTable)
 }
 
-function getParam(story, constraints) {}
+function getParam(story, constraints) {
+  let sortTable = story.getTable('sort')
+  let sessionTable = story.getTable('session')
+  let [height, width] = sortTable.mat.size()
+  let characterIdInOrder = []
+  //console.log(sessionTable)
+  for (let time = 0; time < width; time++) {
+    let num = []
+    for (let id = 0; id < height; id++) {
+      if (sortTable.value(id, time) !== 0) num.push(id)
+    }
+    num.sort((a, b) => {
+      sortTable.value(a, time) - sortTable.value(b, time)
+    })
+    characterIdInOrder.push(num)
+  }
+  //console.log(characterIdInOrder);
+  let rewardArr = []
+  for (let time = 0; time < width - 1; time++) {
+    //the last time do not have to align
+    let reward = []
+    for (let cha = 0; cha < characterIdInOrder[time].length; cha++) {
+      let rewardInEachCha = []
+      for (
+        let chaInNextTime = 0;
+        chaInNextTime < characterIdInOrder[time + 1].length;
+        chaInNextTime++
+      ) {
+        const id1 = characterIdInOrder[time][cha]
+        const id2 = characterIdInOrder[time + 1][chaInNextTime]
+        rewardInEachCha.push(
+          solveReward(id1, id2, time, sessionTable, sortTable)
+        )
+      }
+      reward.push(rewardInEachCha)
+    }
+    rewardArr.push(reward)
+  }
+  return {
+    characterIdInOrder,
+    rewardArr,
+    width,
+    height,
+  }
+}
+function runAlgorithms(param) {
+  //console.log(param)
+  let ans = new Table(-1)
+  let { height, width, rewardArr, characterIdInOrder } = param
+  ans.resize(height, width, -1)
+  for (let time = 0; time < width - 1; time++) {
+    let alignAns
+    alignAns = longestCommonSubstring(
+      characterIdInOrder[time].length,
+      characterIdInOrder[time + 1].length,
+      rewardArr[time]
+    )
+    //console.log("alighAns",alignAns)
+    for (let idOrder = 0; idOrder < alignAns.length; idOrder++) {
+      let alignId = alignAns[idOrder]
+      if (alignAns[idOrder] === undefined) alignId = -1
 
+      ans.replace(characterIdInOrder[time][idOrder], time, alignId)
+    }
+  }
+  //console.log(ans);
+  return ans
+}
+/**
+ *
+ * @param {Number} id1 first character ID
+ * @param {Number} id2 second character ID
+ * @param {Number} time first time
+ * @param {Table} sessionTable sessionTable
+ * @param {Table} sortTable sortTable
+ * @returns {Number} reward
+ */
+function solveReward(id1, id2, time, sessionTable, sortTable) {
+  let reward = 0
+  let sessionId1 = sessionTable.value(id1, time)
+  let sessionId2 = sessionTable.value(id2, time + 1)
+  let [height, width] = sessionTable.mat.size()
+  let session1 = findChaInSessionAtTime(
+    sessionId1,
+    time,
+    sessionTable,
+    sortTable
+  )
+  let session2 = findChaInSessionAtTime(
+    sessionId2,
+    time + 1,
+    sessionTable,
+    sortTable
+  )
+  for (let chaId of session1) {
+    if (session2.indexOf(chaId) !== -1) reward += 1
+  }
+  reward +=
+    alpha *
+    (1 -
+      Math.abs(
+        session1.indexOf(id1) / session1.length -
+          session2.indexOf(id2) / session2.length
+      ))
+  return reward
+}
+
+function findChaInSessionAtTime(sessionId, time, sessionTable, sortTable) {
+  let ans = []
+  let [height, width] = sessionTable.mat.size()
+  for (let id = 0; id < height; id++)
+    if (sessionTable.value(id, time) === sessionId) ans.push(id)
+  ans.sort((a, b) => {
+    return sortTable.value(a, time) - sortTable.value(b, time)
+  })
+  return ans
+}
 /**
  * @param {Number[]} list1
  * @param {Number[]} list2
@@ -35,9 +155,16 @@ export function longestCommonSubstring(list1Length, list2Length, reward) {
   for (let i = 0; i < list1Length; i++) {
     reward[i][-1] = 0
   }
+  reward[-1] = []
+  totalReward[-1] = []
   for (let j = 0; j < list2Length; j++) {
     reward[-1][j] = 0
   }
+  for (let i = -1; i < list1Length; i++)
+    for (let j = -1; j < list2Length; j++) {
+      totalReward[i][j] = 0
+    }
+  //above things are init arrays
   for (let i = 0; i < list1Length; i++) {
     for (let j = 0; j < list2Length; j++) {
       //edge condition
@@ -48,25 +175,26 @@ export function longestCommonSubstring(list1Length, list2Length, reward) {
       ] //0:left,1:right,2:align
       let maxListArg = maxArg(valueList)
       direction[i][j] = maxListArg
+      totalReward[i][j] = valueList[maxListArg]
     }
   }
   let alignList = []
-  let leftHead = list1Length
-  let rightHead = list2Length
+  let leftHead = list1Length - 1
+  let rightHead = list2Length - 1
   while (rightHead >= 0 && leftHead >= 0) {
-    if (direction[i][j] == 2) {
+    if (direction[leftHead][rightHead] == 2) {
       alignList[leftHead] = rightHead
       leftHead -= 1
       rightHead -= 1
-    } else if (direction[i][j] == 1) {
+    } else if (direction[leftHead][rightHead] == 1) {
       rightHead -= 1
-    } else if (direction[i][j] == 0) {
+    } else if (direction[leftHead][rightHead] == 0) {
       leftHead -= 1
     } else {
       break
     }
   }
-  return
+  return alignList
 }
 
 function maxArg(list) {
--- src/js/compact/greedySlotCompact.js
+++ src/js/compact/greedySlotCompact.js
@@ -1,284 +1,348 @@
-let compressTime = [];
-let d1 = 0; //out
-let d2 = 1000; //in
-let graph = {};
-let sequence;
-let data;
-let timeframe = [];
-let slot = [];
-let alignedSession;
-let record = [];
-let mergeInfo = [];
+import { Table } from '../data/table'
 
-//change sequence into timeframe
-function _getTimeframe(time) {
-  let delsessionID = [];
-  let inflag = 1;
-  let timeframe = sequence[time];
-  const Sessions = [];
-  let order = timeframe[1];
-  for (let i = 1; i < order.length; i++) {
-    let session = {};
-    session.begin = timeframe[0];
-    session.end = sequence[time + 1][0] - 1;
-    session.content = order[i][1];
-    let flag = 0;
-    for (let cha in session.content)
-      if (cha.end !== sequence[time][0]) flag = 1;
-    if (flag === 0) continue;
-    for (let i = session.content.length - 1; i >= 0; i--) {
-      if (session.content[i].end === sequence[time][0])
-        session.content.splice(i, 1);
+const DistanceOut = 10
+
+export function greedySlotCompact(story, constraints) {
+  const param = getParam(story, constraints)
+  const layoutTable = runAlgorithms(param)
+  story.setTable('layout', layoutTable)
+}
+
+function getParam(story, constraints) {
+  let alignTable = story.getTable('align')
+  //console.log(alignTable)
+  let sessionTable = story.getTable('session')
+  //console.log(sessionTable);
+  let sortTable = story.getTable('sort')
+  //console.log(sortTable)
+  let [height, width] = sortTable.mat.size()
+  let characterIdInOrder = []
+  ////console.log(sessionTable)
+  for (let time = 0; time < width; time++) {
+    let num = []
+    for (let id = 0; id < height; id++) {
+      if (sortTable.value(id, time) !== 0) num.push(id)
     }
-
-    Sessions.push(session);
-    if (session.content.length == 0) delsessionID.push(i);
+    num.sort((a, b) => {
+      sortTable.value(a, time) - sortTable.value(b, time)
+    })
+    characterIdInOrder.push(num)
   }
-  delsessionID.forEach(sessionID => {
-    order.splice(sessionID, 1);
-  });
-  return Sessions;
+  return {
+    alignTable,
+    sessionTable,
+    sortTable,
+    height,
+    width,
+    characterIdInOrder,
+  }
 }
 
-//find the order of a name in a slot
-function _name2num(time, name) {
-  let ans = -1;
-  let flag = 1;
-  timeframe.forEach(tt => {
-    tt.forEach(x => {
-      if (time >= x.begin && time < x.end) {
-        x.content.forEach(y => {
-          if (y.entity !== name) {
-            ans += flag;
-          } else {
-            ans += flag;
-            flag = 0;
-          }
-        });
-      }
-    });
-  });
-  if (flag === 1) return -1;
-  return ans;
-}
+function runAlgorithms(param) {
+  let {
+    sessionTable,
+    sortTable,
+    alignTable,
+    height,
+    width,
+    characterIdInOrder,
+  } = param
+  let ans = new Table()
+  ans.resize(height, width, -1)
+  let slots = []
+  //init slots
 
-//default function
-//graph is the output
-//data is the names and orders
-//sequence contains keytimes and sessions
-export function greedySlotCompact(
-  alignAns,
-  compressInfo,
-  extendInfo,
-  merge,
-  split,
-  din,
-  dout
-) {
-  // mergeInfo = merge;//merge lines
-  let compactInfo = [];
-  d2 = din;
-  d1 = dout;
-  timeframe = [];
-  record = [];
-  slot = [];
-  compressInfo.forEach(pair => {
-    pair.names.shift();
-    compactInfo.push([pair.names, ...pair.timeSpan, pair.param.scale]);
-  });
-  extendInfo.forEach(pair => {
-    pair.names.shift();
-    compactInfo.push([pair.names, ...pair.timeSpan, pair.param.scale]);
-  });
-  compressInfo = compactInfo;
-  mergeInfo = [];
-  merge.forEach(pair => {
-    mergeInfo.push([pair.names, ...pair.timeSpan]);
-  });
-  data = alignAns;
-  sequence = alignAns.sequence;
-  alignedSession = alignAns.alignedSessions;
-  for (let [_, order] of sequence) {
-    order.unshift(undefined);
-  }
-  let flag = 1;
-  for (let i = 0; i < sequence.length - 1; i++) {
-    slot.push(_getTimeframe(i)); //change sequence into slot
-  }
-  for (let i = 0; i < sequence.length; i++) record[i] = new Map();
-  if (slot.length !== 0)
-    for (let i = 0; i < slot[0].length; i++) {
-      let data = [slot[0][i]];
-      timeframe.push(data);
-      record[0].set(i, timeframe[timeframe.length - 1]);
+  for (let time = 0; time < width; time++) {
+    for (let id = 0; id < height; id++) {
+      if (sortTable.value(id, time) !== -1)
+        ans.replace(id, time, sortTable.value(id, time) * DistanceOut)
     }
-  for (let i = 1; i < slot.length; i++) {
-    _timeframeinsert_new(i);
   }
-  timeframe.forEach(x => {
-    x.forEach(session => {
-      let begin = session.begin;
-      let content = session.content;
-      if (begin !== 1)
-        content.sort(function(a, b) {
-          let aa = _name2num(begin - 1, a.entity);
-          let bb = _name2num(begin - 1, b.entity);
-          if (aa === -1) aa = Number.MAX_VALUE;
-          if (bb === -1) bb = Number.MAX_VALUE;
-          return aa - bb;
-        });
-    });
-  });
-  let node = [];
-  for (let j = 0; j < data.entities.length; j++) {
-    node[j] = [];
-  }
-  let dis = [];
-  for (let i = 0; i < timeframe.length; i++) {
-    let max = 0;
-    for (let j = 0; j < timeframe[i].length; j++) {
-      max =
-        timeframe[i][j].content.length > max
-          ? timeframe[i][j].content.length
-          : max;
-    }
-    dis[i] = max;
-  }
-
-  let Ycoor = new Map();
-
-  for (let j = 0; j < timeframe.length; j++) {
-    let max = 0;
-    for (let key of Ycoor) max = Math.max(max, key[1]);
-    for (let key of Ycoor) Ycoor.set(key[0], max + dout);
-    let t = timeframe[j];
-    t.forEach(x => {
-      let content = x.content;
-
-      for (let ii = 0; ii < content.length; ii++) {
-        let num = 0;
-        let s = 0;
-        // let notEmptyslot = findEmptyslot(t, x.begin, x.end); //之前的非空slot
-        //
-        data.entities.forEach(x => {
-          if (x === content[ii].entity) {
-            num = s;
-          }
-          s++;
-        });
-        content[ii].lineOrder = num;
-        if (content[ii].entity !== "") {
-          let name = content[ii].entity;
-          // if (name=="Wolf") debugger;
-          let compressPair = compressInfo.find(
-            pair =>
-              pair[0].includes(name) && pair[1] <= x.begin && pair[2] >= x.end
-          );
-          let range = 1;
-          let mergePair = mergeInfo.find(
-            pair =>
-              pair[0].includes(name) && pair[1] <= x.begin && pair[2] >= x.end
-          );
-          if (compressPair !== undefined) range = compressPair[3]; //compact调整
-          if (Ycoor.get(x.begin) === undefined) {
-            Ycoor.set(x.begin, max + range * din); //加入一个节点
-            if (mergePair !== undefined) {
-              if (mergePair[0][0] === name) {
-                mergePair[3] = new Map();
-                mergePair[3].set(x.begin, max + range * din); //merge的第一个的坐标
-                node[num].push([x.begin * 50, Ycoor.get(x.begin)]);
-                node[num].push([x.end * 50 + 25, Ycoor.get(x.begin)]);
-              } else {
-                //merge后面的
-                node[num].push([x.begin * 50, mergePair[3].get(x.begin)]);
-                node[num].push([x.end * 50 + 25, mergePair[3].get(x.begin)]);
-              }
-            } else {
-              //正常的
-              node[num].push([x.begin * 50, Ycoor.get(x.begin)]);
-              node[num].push([x.end * 50 + 25, Ycoor.get(x.begin)]);
-            }
-          } else {
-            //已经有了节点的基础，向上累加
-            Ycoor.set(x.begin, Ycoor.get(x.begin) + range * din);
-            if (mergePair !== undefined)
-              if (mergePair[0][0] === name) {
-                mergePair[3] = new Map();
-                mergePair[3].set(x.begin, Ycoor.get(x.begin));
-                node[num].push([x.begin * 50, Ycoor.get(x.begin)]);
-                node[num].push([x.end * 50 + 25, Ycoor.get(x.begin)]);
-              } else {
-                node[num].push([x.begin * 50, mergePair[3].get(x.begin)]);
-                node[num].push([x.end * 50 + 25, mergePair[3].get(x.begin)]);
-              }
-            else {
-              node[num].push([x.begin * 50, Ycoor.get(x.begin)]);
-              node[num].push([x.end * 50 + 25, Ycoor.get(x.begin)]);
-            }
-          }
-        } else {
-          let range = 1;
-          if (Ycoor.get(x.begin) === undefined) {
-            Ycoor.set(x.begin, max + range * din);
-          } else {
-            Ycoor.set(x.begin, Ycoor.get(x.begin) + range * din);
-          }
-        }
-      }
-    });
-  }
-  graph = data;
-  node.forEach(x => x.sort((a, b) => a[0] - b[0]));
-  graph.initialNodes = node;
-  // graph.initNodes=JSON.parse(JSON.stringify(node));
-  // let initialGraph = {};
-  // initialGraph.nodes = node;
-  // initialGraph.names = graph.names;
-  for (let [_, order] of sequence) {
-    order.shift();
-  }
-  return graph;
+  //console.log(ans)
+  return ans
 }
 
-function frame2num(frame) {
-  for (let i = 0; i < timeframe.length; i++) {
-    if (frame === timeframe[i]) return i;
-  }
-  return -1;
-}
+// let compressTime = [];
+// let d1 = 0; //out
+// let d2 = 1000; //in
+// let graph = {};
+// let sequence;
+// let data;
+// let timeframe = [];
+// let slot = [];
+// let alignedSession;
+// let record = [];
+// let mergeInfo = [];
 
-function _timeframeinsert_new(i) {
-  let align = alignedSession[i];
-  let lastrecord = record[i - 1];
-  let thistime = slot[i];
-  let flag = [];
-  thistime.forEach(x => flag.push(0));
-  // debugger
-  if (align[1] === undefined) {
-    for (let j = 0; j < flag.length; j++) {
-      let frame = lastrecord.get(j);
-      frame.push(thistime[j]);
-      record[i].set(j, frame);
-    }
-    return;
-  }
-  align[1].forEach(function(v, k) {
-    let value = v - 1;
-    let key = k - 1;
-    let frame = lastrecord.get(value);
-    record[i].set(key, frame);
-    frame.push(thistime[key]);
-    flag[key] = 1;
-  });
-  if (flag[0] === 0) {
-    timeframe.splice(0, 0, [thistime[0]]);
-    record[i].set(0, timeframe[0]);
-    flag[0] = 1;
-  }
-  for (let j = 1; j < flag.length; j++) {
-    if (flag[j] === 1) continue;
-    let num = frame2num(record[i].get(j - 1));
-    timeframe.splice(num + 1, 0, [thistime[j]]);
-    record[i].set(j, timeframe[num + 1]);
-  }
-}
+// //change sequence into timeframe
+// function _getTimeframe(time) {
+//   let delsessionID = [];
+//   let inflag = 1;
+//   let timeframe = sequence[time];
+//   const Sessions = [];
+//   let order = timeframe[1];
+//   for (let i = 1; i < order.length; i++) {
+//     let session = {};
+//     session.begin = timeframe[0];
+//     session.end = sequence[time + 1][0] - 1;
+//     session.content = order[i][1];
+//     let flag = 0;
+//     for (let cha in session.content)
+//       if (cha.end !== sequence[time][0]) flag = 1;
+//     if (flag === 0) continue;
+//     for (let i = session.content.length - 1; i >= 0; i--) {
+//       if (session.content[i].end === sequence[time][0])
+//         session.content.splice(i, 1);
+//     }
+
+//     Sessions.push(session);
+//     if (session.content.length == 0) delsessionID.push(i);
+//   }
+//   delsessionID.forEach(sessionID => {
+//     order.splice(sessionID, 1);
+//   });
+//   return Sessions;
+// }
+
+// //find the order of a name in a slot
+// function _name2num(time, name) {
+//   let ans = -1;
+//   let flag = 1;
+//   timeframe.forEach(tt => {
+//     tt.forEach(x => {
+//       if (time >= x.begin && time < x.end) {
+//         x.content.forEach(y => {
+//           if (y.entity !== name) {
+//             ans += flag;
+//           } else {
+//             ans += flag;
+//             flag = 0;
+//           }
+//         });
+//       }
+//     });
+//   });
+//   if (flag === 1) return -1;
+//   return ans;
+// }
+
+// //default function
+// //graph is the output
+// //data is the names and orders
+// //sequence contains keytimes and sessions
+// export function greedySlotCompact(
+//   alignAns,
+//   compressInfo,
+//   extendInfo,
+//   merge,
+//   split,
+//   din,
+//   dout
+// ) {
+//   // mergeInfo = merge;//merge lines
+//   let compactInfo = [];
+//   d2 = din;
+//   d1 = dout;
+//   timeframe = [];
+//   record = [];
+//   slot = [];
+//   compressInfo.forEach(pair => {
+//     pair.names.shift();
+//     compactInfo.push([pair.names, ...pair.timeSpan, pair.param.scale]);
+//   });
+//   extendInfo.forEach(pair => {
+//     pair.names.shift();
+//     compactInfo.push([pair.names, ...pair.timeSpan, pair.param.scale]);
+//   });
+//   compressInfo = compactInfo;
+//   mergeInfo = [];
+//   merge.forEach(pair => {
+//     mergeInfo.push([pair.names, ...pair.timeSpan]);
+//   });
+//   data = alignAns;
+//   sequence = alignAns.sequence;
+//   alignedSession = alignAns.alignedSessions;
+//   for (let [_, order] of sequence) {
+//     order.unshift(undefined);
+//   }
+//   let flag = 1;
+//   for (let i = 0; i < sequence.length - 1; i++) {
+//     slot.push(_getTimeframe(i)); //change sequence into slot
+//   }
+//   for (let i = 0; i < sequence.length; i++) record[i] = new Map();
+//   if (slot.length !== 0)
+//     for (let i = 0; i < slot[0].length; i++) {
+//       let data = [slot[0][i]];
+//       timeframe.push(data);
+//       record[0].set(i, timeframe[timeframe.length - 1]);
+//     }
+//   for (let i = 1; i < slot.length; i++) {
+//     _timeframeinsert_new(i);
+//   }
+//   timeframe.forEach(x => {
+//     x.forEach(session => {
+//       let begin = session.begin;
+//       let content = session.content;
+//       if (begin !== 1)
+//         content.sort(function(a, b) {
+//           let aa = _name2num(begin - 1, a.entity);
+//           let bb = _name2num(begin - 1, b.entity);
+//           if (aa === -1) aa = Number.MAX_VALUE;
+//           if (bb === -1) bb = Number.MAX_VALUE;
+//           return aa - bb;
+//         });
+//     });
+//   });
+//   let node = [];
+//   for (let j = 0; j < data.entities.length; j++) {
+//     node[j] = [];
+//   }
+//   let dis = [];
+//   for (let i = 0; i < timeframe.length; i++) {
+//     let max = 0;
+//     for (let j = 0; j < timeframe[i].length; j++) {
+//       max =
+//         timeframe[i][j].content.length > max
+//           ? timeframe[i][j].content.length
+//           : max;
+//     }
+//     dis[i] = max;
+//   }
+
+//   let Ycoor = new Map();
+
+//   for (let j = 0; j < timeframe.length; j++) {
+//     let max = 0;
+//     for (let key of Ycoor) max = Math.max(max, key[1]);
+//     for (let key of Ycoor) Ycoor.set(key[0], max + dout);
+//     let t = timeframe[j];
+//     t.forEach(x => {
+//       let content = x.content;
+
+//       for (let ii = 0; ii < content.length; ii++) {
+//         let num = 0;
+//         let s = 0;
+//         // let notEmptyslot = findEmptyslot(t, x.begin, x.end); //之前的非空slot
+//         //
+//         data.entities.forEach(x => {
+//           if (x === content[ii].entity) {
+//             num = s;
+//           }
+//           s++;
+//         });
+//         content[ii].lineOrder = num;
+//         if (content[ii].entity !== "") {
+//           let name = content[ii].entity;
+//           // if (name=="Wolf") debugger;
+//           let compressPair = compressInfo.find(
+//             pair =>
+//               pair[0].includes(name) && pair[1] <= x.begin && pair[2] >= x.end
+//           );
+//           let range = 1;
+//           let mergePair = mergeInfo.find(
+//             pair =>
+//               pair[0].includes(name) && pair[1] <= x.begin && pair[2] >= x.end
+//           );
+//           if (compressPair !== undefined) range = compressPair[3]; //compact调整
+//           if (Ycoor.get(x.begin) === undefined) {
+//             Ycoor.set(x.begin, max + range * din); //加入一个节点
+//             if (mergePair !== undefined) {
+//               if (mergePair[0][0] === name) {
+//                 mergePair[3] = new Map();
+//                 mergePair[3].set(x.begin, max + range * din); //merge的第一个的坐标
+//                 node[num].push([x.begin * 50, Ycoor.get(x.begin)]);
+//                 node[num].push([x.end * 50 + 25, Ycoor.get(x.begin)]);
+//               } else {
+//                 //merge后面的
+//                 node[num].push([x.begin * 50, mergePair[3].get(x.begin)]);
+//                 node[num].push([x.end * 50 + 25, mergePair[3].get(x.begin)]);
+//               }
+//             } else {
+//               //正常的
+//               node[num].push([x.begin * 50, Ycoor.get(x.begin)]);
+//               node[num].push([x.end * 50 + 25, Ycoor.get(x.begin)]);
+//             }
+//           } else {
+//             //已经有了节点的基础，向上累加
+//             Ycoor.set(x.begin, Ycoor.get(x.begin) + range * din);
+//             if (mergePair !== undefined)
+//               if (mergePair[0][0] === name) {
+//                 mergePair[3] = new Map();
+//                 mergePair[3].set(x.begin, Ycoor.get(x.begin));
+//                 node[num].push([x.begin * 50, Ycoor.get(x.begin)]);
+//                 node[num].push([x.end * 50 + 25, Ycoor.get(x.begin)]);
+//               } else {
+//                 node[num].push([x.begin * 50, mergePair[3].get(x.begin)]);
+//                 node[num].push([x.end * 50 + 25, mergePair[3].get(x.begin)]);
+//               }
+//             else {
+//               node[num].push([x.begin * 50, Ycoor.get(x.begin)]);
+//               node[num].push([x.end * 50 + 25, Ycoor.get(x.begin)]);
+//             }
+//           }
+//         } else {
+//           let range = 1;
+//           if (Ycoor.get(x.begin) === undefined) {
+//             Ycoor.set(x.begin, max + range * din);
+//           } else {
+//             Ycoor.set(x.begin, Ycoor.get(x.begin) + range * din);
+//           }
+//         }
+//       }
+//     });
+//   }
+//   graph = data;
+//   node.forEach(x => x.sort((a, b) => a[0] - b[0]));
+//   graph.initialNodes = node;
+//   // graph.initNodes=JSON.parse(JSON.stringify(node));
+//   // let initialGraph = {};
+//   // initialGraph.nodes = node;
+//   // initialGraph.names = graph.names;
+//   for (let [_, order] of sequence) {
+//     order.shift();
+//   }
+//   return graph;
+// }
+
+// function frame2num(frame) {
+//   for (let i = 0; i < timeframe.length; i++) {
+//     if (frame === timeframe[i]) return i;
+//   }
+//   return -1;
+// }
+
+// function _timeframeinsert_new(i) {
+//   let align = alignedSession[i];
+//   let lastrecord = record[i - 1];
+//   let thistime = slot[i];
+//   let flag = [];
+//   thistime.forEach(x => flag.push(0));
+//   // debugger
+//   if (align[1] === undefined) {
+//     for (let j = 0; j < flag.length; j++) {
+//       let frame = lastrecord.get(j);
+//       frame.push(thistime[j]);
+//       record[i].set(j, frame);
+//     }
+//     return;
+//   }
+//   align[1].forEach(function(v, k) {
+//     let value = v - 1;
+//     let key = k - 1;
+//     let frame = lastrecord.get(value);
+//     record[i].set(key, frame);
+//     frame.push(thistime[key]);
+//     flag[key] = 1;
+//   });
+//   if (flag[0] === 0) {
+//     timeframe.splice(0, 0, [thistime[0]]);
+//     record[i].set(0, timeframe[0]);
+//     flag[0] = 1;
+//   }
+//   for (let j = 1; j < flag.length; j++) {
+//     if (flag[j] === 1) continue;
+//     let num = frame2num(record[i].get(j - 1));
+//     timeframe.splice(num + 1, 0, [thistime[j]]);
+//     record[i].set(j, timeframe[num + 1]);
+//   }
+// }
