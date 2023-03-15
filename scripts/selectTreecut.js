20b9569fbb1330bcf499370e20823c33b5153e2f
yan1 <tangtan1012@gmail.com>
2023-03-15 18:19:14 +0800

    fix: fix root description length

--- scripts/selectTreecut.js
+++ scripts/selectTreecut.js
@@ -2,27 +2,41 @@ const { clone } = require('lodash')
 
 // calculate parameter description length
 function calculatePar(timeline, nodes) {
-  const S = timeline[timeline.length - 1] - timeline[0]
-  const K = nodes[nodes.length - 1].value[1] - nodes[0].value[0]
+  // const S = timeline[timeline.length - 1] - timeline[0]
+  // const K = nodes[nodes.length - 1].value[1] - nodes[0].value[0]
+  const S = timeline.length - 1
+  const K = nodes.length
   const res = (K / 2) * Math.log2(S)
   return res
 }
 
+const P_hat = (n, timeline) => {
+  const nDuration = n.value[1] - n.value[0]
+  const totalDuration = timeline[timeline.length - 1] - timeline[0]
+  return nDuration / totalDuration
+}
+
 // calculate data description length
 function calculateDat(timeline, nodes) {
-  const P_hat = n => {
-    const nDuration = n.value[1] - n.value[0]
-    const totalDuration = timeline[timeline.length - 1] - timeline[0]
-    return nDuration / totalDuration
-  }
-  const res = -nodes.reduce((sum, n) => sum + Math.log(P_hat(n)), 0)
-  return res
+  return -nodes.reduce((sum, n) => sum + Math.log2(P_hat(n, timeline)), 0)
+}
+
+// calculate data description length for the node itself
+function calculateRootDat(timeline, nodes) {
+  const rootNode = nodes[0]
+  const rootChildrenNum = rootNode.children.length
+  // assume evenly distribution
+  const rootP = P_hat(rootNode, timeline)
+  return -rootChildrenNum * Math.log2(rootP / rootChildrenNum)
 }
 
 // calculate description length
 function calculateDescriptionLength(timeline, nodes) {
-  const result = calculatePar(timeline, nodes) + calculateDat(timeline, nodes)
-  return result
+  return calculatePar(timeline, nodes) + calculateDat(timeline, nodes)
+}
+
+function calculateRootDescriptionLength(timeline, nodes) {
+  return calculatePar(timeline, nodes) + calculateRootDat(timeline, nodes)
 }
 
 const findMDL = (timeline, tree) => {
@@ -39,7 +53,8 @@ const findMDL = (timeline, tree) => {
   }
 
   // Check if collapsing the lower-level optimal models reduces the description length
-  const rootModelLength = calculateDescriptionLength(timeline, [tree])
+  const rootModelLength = calculateRootDescriptionLength(timeline, [tree])
+  // const rootModelLength = calculateDescriptionLength(timeline, [tree])
   const optimalModelLength = calculateDescriptionLength(timeline, optimalModels)
   console.log('rootModelLength :>> ', rootModelLength)
   console.log('optimalModelLength :>> ', optimalModelLength)
